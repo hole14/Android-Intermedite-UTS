ee908d3a7095b72d38fc57c61c1e2c6057f2cfab
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-07 22:07:06 +0700

    Mengubah bottom navigation ketika diklik

--- app/src/main/java/com/example/myprojectuts/MainActivity.kt
+++ app/src/main/java/com/example/myprojectuts/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.myprojectuts
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myprojectuts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
